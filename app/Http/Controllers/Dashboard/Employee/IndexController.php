b40142febebd886ce8f6494068e03cbc7bc65368
M. Hosseyn Najafi <hosseynjf@icloud.com>
2021-08-31 17:01:57 +0430

    Redirect employee dashboard index to task manage

--- app/Http/Controllers/Dashboard/Employee/IndexController.php
+++ app/Http/Controllers/Dashboard/Employee/IndexController.php
@@ -10,7 +10,7 @@ use App\Models\Project;
 use App\Models\Phase;
 use App\Models\EmployeeProject;
 use App\Models\message;
-use Illuminate\Auth\Access\Gate; 
+use Illuminate\Auth\Access\Gate;
 use Illuminate\Support\Facades\Auth;
 use phpDocumentor\Reflection\Types\Null_;
 use Illuminate\Support\Facades\Storage;
@@ -19,15 +19,13 @@ use App\Http\Controllers\Controller;
 
 class IndexController extends Controller
 {
-    public function profile() { 
+    public function profile() {
         $message=message::where('user_id', Auth::user()->id)->orderBy('created_at', 'desc')->get();
         $employee=EmployeeProject::where('employee_id',Auth::user()->id)->orderBy('created_at', 'desc')->get();
         $task=Task::where('employee_id',Auth::user()->id)->orderBy('finish_date', 'ASC')->get();
         return view('dashboard.employee.index', ['employee' => $employee , 'task' => $task , 'message' => $message]);
     }
-        public function get() { 
-        $message=message::where('user_id', Auth::user()->id)->orderBy('created_at', 'desc')->get();
-        $task=Task::where('status','notwork')->where('employee_id',Auth::user()->id)->orderBy('finish_date', 'ASC')->get();
-        return view('dashboard.employee.task.manage', ['task' => $task , 'message' => $message]);
+        public function get() {
+        return redirect()->route('dashboard.employee.task.manage');
     }
 }
