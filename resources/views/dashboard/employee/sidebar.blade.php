aeb9d2caec839f20218a9395727005a01074743e
M. Hosseyn Najafi <hosseynjf@icloud.com>
2021-09-11 01:44:46 +0430

    Move profile element in sidebar to the bottom

--- resources/views/dashboard/employee/sidebar.blade.php
+++ resources/views/dashboard/employee/sidebar.blade.php
@@ -1,5 +1,5 @@
-﻿<x-sidebar-item title="ویرایش مشخصات" icon="fas fa-user" route="dashboard.profile.edit" />
-<x-sidebar-item title="پروفایل" icon="fas fa-tachometer-alt" route="dashboard.employee.profile" />
+﻿<x-sidebar-item title="پروفایل" icon="fas fa-tachometer-alt" route="dashboard.employee.profile" />
 <x-sidebar-item title="مدیریت مسئولیت ها" icon="fas fa-folder" route="dashboard.employee.task.manage" />
 <x-sidebar-item title="مدیریت مالی" icon="fas fa-money-check-alt" route="dashboard.employee.money.index" />
 <x-sidebar-item title="پیام ها" icon="fas fa-envelope-open-text" route="dashboard.employee.message.manage"/>
+<x-sidebar-item title="ویرایش مشخصات" icon="fas fa-user" route="dashboard.profile.edit" />
