f8b132124c7919622f3676e0c5b2680e143d60a5
M. Hosseyn Najafi <hosseynjf@icloud.com>
2021-09-11 01:38:50 +0430

    Revert "Remove profile image from dashboard sidebar"
    This reverts commit ab853fa7

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -90,6 +90,9 @@ BY WEBITO
         <div class="sidebar">
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                <div class="image">
+                    <img src="{{ asset('assets/dashboard/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                </div>
                 <div class="info">
                     <a href="#" class="d-block">{{ Auth::user()->first_name }} {{ Auth::user()->last_name }}</a>
                 </div>
