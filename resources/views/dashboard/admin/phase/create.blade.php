1a9d1696de8f6347983c25690d5d1c6bb1897da2
M. Hosseyn Najafi <hosseynjf@icloud.com>
2021-09-18 18:33:03 +0430

    Fix phase creation modal title

--- resources/views/dashboard/admin/phase/create.blade.php
+++ resources/views/dashboard/admin/phase/create.blade.php
@@ -4,7 +4,7 @@
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title">ویرایش فاز پروژه</h4>
+                    <h4 class="modal-title">ساخت فاز جدید</h4>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
